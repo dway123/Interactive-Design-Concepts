5d6110394bd3bbb97d64cd22719a78d22de8ac86
Darwin Huang <huangdarwin2@gmail.com>
2017-04-08 22:38:15 -0400

    players have turrets wow

--- 4-shooter/js/game.js
+++ 4-shooter/js/game.js
@@ -4,31 +4,54 @@ game.init();
 function Game(){
 	var canvas;
 	var context;
+	var text;
 
 	var player;
 
 	var backgroundMusic;
 	var backgroundColor = "white";
 
+	//reference for jquery mouse movement
+	//http://stackoverflow.com/questions/7298507/move-element-with-keypress-multiple
+	var keys = {};
+
+
 	this.init = function(){
+		//canvas setup
 		canvas = document.getElementById("canvas");
 		context = canvas.getContext("2d");
 		context.canvas.width  = window.innerWidth;
 		context.canvas.height = window.innerHeight;
 
+		//background music setup
 		backgroundMusic = new Audio("audio/music.mp3");
 		backgroundMusic.loop = true;
-		backgroundMusic.play();
+		// backgroundMusic.play();
 
+		//keys setup
+		$(document).keydown(function(e) {
+		    keys[e.keyCode] = true;
+		});
+
+		$(document).keyup(function(e) {
+		    delete keys[e.keyCode];
+		});
+
+		//objects setup
 		player = new Player(canvas.width/2, canvas.height/2, context);
+		text = new Text(context);
+
+		//begin rendering
 		render();
 	}
 
 	function render(){
 		context.fillStyle = backgroundColor;
 		context.fillRect(0, 0, canvas.width, canvas.height);
-
+		player.input(keys);
+		player.move();
 		player.render();
+		text.drawMiddleBottom("Player lives: " + player.getLives());
 		requestAnimationFrame(render);
 	}
 }
--- 4-shooter/js/player.js
+++ 4-shooter/js/player.js
@@ -1,30 +1,111 @@
 function Player(origX, origY, ctx){
-	const pdx = 5;
-	const pd2x = dx/20;
+	const moveSpeed = 10;
+	const rotationSpeed = 5;
 	
-	var x = origX;
-	var y = origY;
-	var dx = 0;
-	var dy = 0;
-	var ballRadius = 5;
+	var location = {
+		x: origX,
+		y: origY
+	};
+
+	var velocity = {
+		x: 0,
+		y: 0
+	}
+
+	var ballRadius = 15;
 	var lives = 3;
-	var color = "black";
+	var color = "red";
 	var context = ctx;
+	var angle = 0;	//rotation about z axis
+
+	var turret = {
+		l: 10,
+		w: 25,
+		color: "black"
+	};
 
 	this.render = function(){
+		context.save();
+
+	    context.translate(location.x, location.y);
+	    context.rotate( angle*Math.PI/180 ); 
+	    context.translate(-location.x, -location.y);
+
+	   	context.fillStyle = turret.color;
+		context.fillRect(location.x, location.y - turret.l/2, turret.w, turret.l);
+
 		context.beginPath();
-	    context.arc(x, y, ballRadius, 0, Math.PI*2);
+	    context.arc(location.x, location.y, ballRadius, 0, Math.PI*2);
 	    context.fillStyle = color;
 	    context.fill();
 	    context.closePath();
+
+	    context.restore();
 	}
 	this.shoot = function(){
-
+		console.log("shoot");
 	}
+	this.input = function(keys){
+		velocity = {
+			x: 0,
+			y: 0
+		};
+		//check Pressed keys
+		for (var key in keys) {
+	        if (!keys.hasOwnProperty(key)) continue;
+	        if (key == 37 || key == 65) {	//left/a
+	            velocity.x -= moveSpeed;
+	        }
+	        else if (key == 39 || key == 68) {	//right/d
+	            velocity.x += moveSpeed;
+	        }
+	        
+	        if (key == 38 || key == 87) {	//up/w
+	            velocity.y -= moveSpeed;
+	        }
+	        else if (key == 40 || key == 83) {	//down/s
+	           	velocity.y += moveSpeed;
+	        }
+
+	        if(key == 81){	//q
+	        	angle = (angle - rotationSpeed) % 360;
+	        }
+	        if(key == 69){	//e
+	        	angle = (angle + rotationSpeed) % 360;
+	        }
+
+	        if(key == 32){	//space
+	        	this.shoot();
+	        }
+	    }
+	}
+
 	this.move = function(){
+	    const wallOffset = {
+	    	x: 0,
+	    	y: moveSpeed/2
+	    }
+	    //todo: collision detection here!!!
+	    if(location.x + velocity.x - ballRadius < 0 + wallOffset.x){						//left wall
+	    	velocity.x = Math.abs(velocity.x);
+	    }
+	    if(location.x + velocity.x + ballRadius > context.canvas.width - wallOffset.x) {	//right wall
+	        velocity.x = -Math.abs(velocity.x);
+	    }
+	    if(location.y + velocity.x - ballRadius < 0 + wallOffset.y){						//upper wall
+	    	velocity.y = Math.abs(velocity.y);
+	    }
+	    if(location.y + velocity.x + ballRadius > context.canvas.height - wallOffset.y) {	//lower wall
+	        velocity.y = -Math.abs(velocity.y);
+	    }
 
-	}
+	    location.x += velocity.x;
+	    location.y += velocity.y;
+	}	
 	this.die = function(){
-
+		lives--;
+	}
+	this.getLives = function(){
+		return lives;
 	}
 }
